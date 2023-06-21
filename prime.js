dbbc0886c694a4194c4c1a4101e3780234168421
sphinx010 <123270719+sphinx010@users.noreply.github.com>
2023-06-21 20:03:13 +0100

    Update prime.js

--- prime.js
+++ prime.js
@@ -15,39 +15,6 @@ slides[slideIndex-1].style.display = "block";
 setTimeout(showSlides, 3000);
 }
 
-// // Get references to the menu item containers
-// const smartphonesContainer = document.querySelector('.batch1');
-// const laptopsContainer = document.querySelector('.batch2');
-// const gadgetsContainer = document.querySelector('.batch3');
-
-// // Get references to the navigation links
-// const smartphonesLink = document.getElementById('smartphones-link');
-// const laptopsLink = document.getElementById('laptops-link');
-// const gadgetsLink = document.getElementById('gadgets-link');
-
-// // Function to show the selected menu items and hide others
-// function showMenuItems(containerToShow, containerToHide1, containerToHide2) {
-//   containerToShow.style.display = 'grid';
-//   containerToHide1.style.display = 'none';
-//   containerToHide2.style.display = 'none';
-// }
-
-// // Add click event listeners to the navigation links
-// smartphonesLink.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   showMenuItems(smartphonesContainer, laptopsContainer, gadgetsContainer);
-// });
-
-// laptopsLink.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   showMenuItems(laptopsContainer, smartphonesContainer, gadgetsContainer);
-// });
-
-// gadgetsLink.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   showMenuItems(gadgetsContainer, smartphonesContainer, laptopsContainer);
-// });
-
 
 // Get references to the menu item containers
 const smartphonesContainer = document.querySelector('.batch1');
